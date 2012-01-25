b295167939c1ea473c8cb8a0e8177545c99fa353
Bovard Tiberi <bovard.tiberi@gmail.com>
2012-01-24 22:57:05 -0700

    Added a basic navigation system

---
+++ NavigationMode.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team147;
+
+/**
+ *
+ * @author Bovard
+ */
+public class NavigationMode {
+    
+}
